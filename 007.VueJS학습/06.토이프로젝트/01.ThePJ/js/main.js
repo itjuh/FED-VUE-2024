3b2a4d2cb6604724260ac7ae36b4babf2cbcdaa7
itjuh <jh.2144.9679@gmail.com>
2024-01-24 18:27:11 +0900

    토이프로젝트 구성중

--- 007.VueJS학습/06.토이프로젝트/01.ThePJ/js/main.js
+++ 007.VueJS학습/06.토이프로젝트/01.ThePJ/js/main.js
@@ -10,35 +10,55 @@ Vue.component("nav-comp", {
   template: `
         <li v-text="menu"></li>
     `,
-  data:function(){
-    return{
-        menu:`${this.gnbMenu()}`,
+  data: function () {
+    return {
+      menu: `${this.gnbMenu()}`,
     };
   },
-  methods:{
-    gnbMenu(){
-        i++;
-        return navList[i];
+  methods: {
+    gnbMenu() {
+      i++;
+      return navList[i];
     },
-  }
+  },
 }); // 메뉴 컴포넌트
-makeVue('.nav ul');
+makeVue(".nav ul");
 
 // 뷰 로고 컴포넌트
-Vue.component('logo-comp',{
-    template:`
-        <li><img v-bind:src='logosrc' alt='더진한로고이미지' /></li>
+Vue.component("logo-comp", {
+  template: `
+        <div><img v-bind:src='logosrc' alt='더진한로고이미지' /></div>
     `,
-    data(){
-        return{
-            logosrc:`./images/theBase/001.png`
-        }
-    },
+  data() {
+    return {
+      logosrc: `./images/theBase/001.png`,
+    };
+  },
 }); // 로고 컴포넌트
-makeVue('.nav ul');
+makeVue(".nav ul");
 
 // 뷰 검색창 컴포넌트
-Vue.component('search-comp',{
-    template:``,
-    
+Vue.component("sch-comp", {
+  template: `
+    <div class='sch-box'>
+    <input type="text" name="sch" id="sch-box" maxlength="10">
+    </input>
+    <button><i class="fa-solid fa-magnifying-glass"></i></button>
+    <div>
+    `,
+}); // 검색창 컴포넌트
+makeVue(".nav ul");
+
+// 뷰 햄버거버튼 컴포넌트
+Vue.component("side-comp", {
+  template: `
+    <div>
+        <a href="" class="moblie-btn">
+            <div></div>
+            <div></div>
+            <div></div>
+        </a>
+    </div>
+    `,
 });
+makeVue(".nav ul");
