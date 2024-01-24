84f869bc91ba25871aa01a989dfc875bb4a96ce4
itjuh <jh.2144.9679@gmail.com>
2024-01-24 14:24:20 +0900

    컴포넌트 만들기2

--- 007.VueJS학습/02.기초연습/js/comp1.js
+++ 007.VueJS학습/02.기초연습/js/comp1.js
@@ -1,6 +1,6 @@
 // 01. 컴포넌트 연습 JS
 
-// 뷰JS 인스턴스 생성용 함수
+// 뷰JS 인스턴스 생성용 함수 : 부모컴포넌트
 const makeVue = (x) => new Vue({ el: x }); // x는 대상요소
 
 // 1. 제목에 넣을 전역 컴포넌트 만들기
@@ -19,6 +19,12 @@ Vue.component("tit-comp", {
 // 컴포넌트를 먼저 만들고 나서 뷰 인스턴스 생성하기
 makeVue(".tit");
 
+// 이미지 번호 숫자 증감 변수
+let inum = 0;
+// 상품명 배열
+const goods = ["프레이컷", "아일렛기모", "베어부클", "포멀믹스톤"];
+
+
 // 2. 그리드 레이아웃 상품박스 컴포넌트 만들기
 Vue.component("list-comp", {
   // 2-1. template옵션 : 태그 구성을 잡아줌
@@ -28,8 +34,8 @@ Vue.component("list-comp", {
     <div>
         <img v-bind:src="gsrc" alt="dress" />
         <aside>
-          <h2>상품명1</h2>
-          <h3>상품가격1</h3>
+          <h2 v-text='gname'></h2>
+          <h3 v-text='gprice'></h3>
         </aside>
     </div>
   `,
@@ -41,20 +47,62 @@ Vue.component("list-comp", {
     // 속성:값
     return{
         // 이미지경로
-        gsrc:`images/47.jpg`,
+        gsrc:`images/${this.setNum()}.jpg`,
         // 상품명
-        gname:``,
+        gname:`${this.setName()}`,
         // 상품가
-        gprice:``,
+        gprice:`￦${this.setPrice()}`,
     }
   },
   // 2-3. methods옵션 : 컴포넌트 내부 메서드 세팅
   methods: {
+    // inum을 1씩 증가하여 리턴함
     setNum(){
-        
+        inum +=1;
+        console.log('inum:',inum);
+        return inum;
+    },
+    setName(){
+        // 상품명 랜덤수
+        let randomGoodsNum = Math.floor(Math.random() * 4);
+        return goods[randomGoodsNum];
+    },
+    setPrice(){
+        // 가격 랜덤수 4~20사이 난수 곱
+        let randomPriceNum = Math.ceil(Math.random() * 17) + 3;
+        return this.numberWithCommas(randomPriceNum+'0000');
+    },
+    // 정규식 함수(숫자 세자리마다 콤마해주는 기능)
+    numberWithCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     },
   },
 }); // 전역 컴포넌트 2
 
 // 리스트뷰 인스턴스 생성하기
 makeVue('.grid');
+
+// 유튜브 아이프레임 컴포넌트 //
+Vue.component('ifr-comp',{
+    template:`
+        <iframe
+        width="49%"
+        style="aspect-ratio: 16/9"
+        v-bind:src="isrc"
+        v-bind:title="itit"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+         ></iframe>
+    `,
+    // data:function(){
+    data(){
+        return{
+            isrc:`https://www.youtube.com/embed/ZH1Y1l1OmTY?autoplay=1&mute=1&loop=1&playlist=ZH1Y1l1OmTY`,
+            itit:`#고윤정 과 함께 차가운 겨울을 더욱 액티브하게!  l 디스커버리 23FW #goyounjung #크롭패딩`,
+        }
+    },
+});
+
+// 부모컴포넌트 인스턴스 생성하기 //
+makeVue('.iframe-box');
